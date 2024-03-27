34decbe11e171d0daefae5cc57004b6b2666dfa6
Christiano Justus <justus@positivo.com.br>
2024-03-27 19:21:51 -0300

    ajustes

--- pages/POC 06 - Haystack.py
+++ pages/POC 06 - Haystack.py
@@ -4,28 +4,27 @@ import os
 from haystack import Pipeline, PredefinedPipeline
 
 
-
+st.title("Haystack")
+# Inicializa Page
+util.init_page()
 
 # Inicializa o cliente
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 def execute(url, pergunta):
-    body()
     pipeline = Pipeline.from_template(PredefinedPipeline.CHAT_WITH_WEBSITE)
-    with st.spinner('Executanto ...'):
-        result = pipeline.run({
-            "fetcher": {"urls": [url]},
-            "prompt": {"query": pergunta}}
-        )
-        st.write(result["llm"]["replies"][0])
+    try:
+        with st.spinner('Executanto ...'):
+            result = pipeline.run({
+                "fetcher": {"urls": [url]},
+                "prompt": {"query": pergunta}}
+            )
+            st.write(result["llm"]["replies"][0])
+    except Exception as e:
+        st.error(f"Erro: {e}")
 
-def body():
-    st.title("Haystack")
-    # Inicializa Page
-    util.init_page()
-    st.caption('Executanto teste com Haystack para responder uma pergunta sobre um site.')
-    url = st.text_input('Digite a URL do site:', 'https://haystack.deepset.ai/overview/quick-start')
-    pergunta = st.text_input('Digite a pergunta:', 'Which components do I need for a RAG pipeline?')
-    st.button('Executar', on_click=execute, args=(url, pergunta))
-
-body()
+    
+st.caption('Executanto teste com Haystack para responder uma pergunta sobre um site.')
+url = st.text_input('Digite a URL do site:', 'https://educacional.com.br/')
+pergunta = st.text_input('Digite a pergunta:', 'Quais são as soluções educacionais?')
+st.button('Executar', on_click=execute, args=(url, pergunta))
